469726915c8746a25229493488f736453d10650c
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-10-21 22:43:14 -0400

    added function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react"
 import Blogs from "./components/Blogs/Blogs"
 import Bookmarks from "./components/Bookmarks/Bookmarks"
 import Header from "./components/Header/Header"
 
 function App() {
 
+  // আমরা এখন বুকমার্ক অংশে সিলেক্টেড ব্লগগুলোকে পাঠানোর জন্য এখানে লেভেল আপ করব। স্টেট ডিক্লেয়ার করব। এটাকে বলা হয় lift up the state
+
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleAddToBookmark = blog => {
+    // console.log('bookmark adding soon');
+    // console.log(blog);
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+
+  const [readingTime, setReadingTime] = useState(0);
+  // here, the default value of total reading time is set to zero.
+
+  const handleMarkAsRead = (id, time) => {
+    // console.log('marking as read', time);
+    setReadingTime(readingTime + time)
+    // remove the read blog from bookmark
+    console.log('remove bookmark', id);
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id!==id);
+    setBookmarks(remainingBookmarks)
+  }
 
   return (
     <>
       <Header></Header>
-      <div className="md:flex justify-between items-center">
-        <Blogs></Blogs>
-        <Bookmarks></Bookmarks>
+      <div className="md:flex max-w-7xl mx-auto">
+        <Blogs handleAddToBookmark = {handleAddToBookmark} handleMarkAsRead = {handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks ={bookmarks} readingTime ={readingTime}></Bookmarks>
       </div>
     </>
   )
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,44 @@
 import PropTypes from 'prop-types';
+import { FaRegBookmark } from "react-icons/fa";
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    // console.log(blog);
+    const {id, title, cover, author, author_img, reading_time, posted_date, hashtags } = blog;
     return (
-        <div>
-            
+        <div className='mb-20 space-y-4'>
+            <img className='w-full mb-8 rounded-lg' src={cover} alt={`cover picture of the title- ${title}`} />
+            <div className='flex justify-between mb-4 '>
+                <div className='flex '>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)}
+                        className='ml-2 text-red-400 text-2xl'><FaRegBookmark></FaRegBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title} </h2>
+            <p>
+                {/* এখানে hashtagগুলো array আকারে আছে। তাই ম্যাপ করে পেলাম। যেহেতু তাদের আলাদা কোনো আইডি ছিল না, তাই ম্যাপের আরেকটা প্যারামিটার ব্যবহার করলাম যেখানে ম্যাপ লিস্টের এলিমেন্টগুলোর ইন্ডেক্সকে ব্যবহার করে */}
+                {
+                    hashtags.map((hashtag, idx) => <span key={idx}><a href="">#{hashtag}</a></span>)
+                }
+            </p>
+            <button
+            onClick={()=>handleMarkAsRead(id, reading_time)}
+            className='text-purple-600 font-bold underline'>Mark As Read</button>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func.isRequired,
+    handleMarkAsRead: PropTypes.func.isRequired
 }
 
 export default Blog;
